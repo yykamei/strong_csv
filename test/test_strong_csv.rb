a90cb0c236c7eb4f37cf66ccf3c5a509551b2fa2
Yutaka Kamei <kamei@yykamei.me>
2022-04-20 23:19:56 +0900

    Test!

--- test/test_strong_csv.rb
+++ test/test_strong_csv.rb
@@ -2,12 +2,8 @@
 
 require "test_helper"
 
-class TestStrongCsv < Minitest::Test
+class TestStrongCSV < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::StrongCsv::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+    refute_nil ::StrongCSV::VERSION
   end
 end
