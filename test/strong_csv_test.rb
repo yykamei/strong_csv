cc55601bb92b355eab9855b33373b9d0ab5c1aff
Yutaka Kamei <kamei@yykamei.me>
2022-04-26 09:52:46 +0900

    Rename test_strong_csv.rb to strong_csv_test.rb

--- test/strong_csv_test.rb
+++ test/strong_csv_test.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class TestStrongCSV < Minitest::Test
+class StrongCSVTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil StrongCSV::VERSION
   end
