6a43c0b40f90e3c7f29f08fd539fc00799b623fd
Yutaka Kamei <kamei@yykamei.me>
2022-04-28 21:34:23 +0900

    Prefix :: to avoid ambiguous constant resolution (#24)

--- lib/strong_csv/types/literal.rb
+++ lib/strong_csv/types/literal.rb
@@ -20,7 +20,7 @@ class StrongCSV
         end
       end
 
-      refine Range do
+      refine ::Range do
         # @param value [Object] Value to be casted to Integer
         # @return [ValueResult]
         def cast(value)
@@ -46,7 +46,7 @@ class StrongCSV
         end
       end
 
-      refine String do
+      refine ::String do
         # @param value [Object] Value to be casted to String
         # @return [ValueResult]
         def cast(value)
